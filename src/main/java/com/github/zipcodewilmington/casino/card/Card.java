a67bafc43e16fef0b88de86c7bcbfeffdbf12b95
jgiroso <jrgiroso@gmail.com>
2021-07-13 21:14:58 -0400

    messed up cards

--- src/main/java/com/github/zipcodewilmington/casino/card/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/card/Card.java
@@ -1,4 +1,40 @@
 package com.github.zipcodewilmington.casino.card;
 
-public enum Card {
+public class Card {
+    private cardValue cardValue;
+    private suitValue suitValue;
+
+    public Card(suitValue suitValue, cardValue cardValue){
+        this.cardValue = cardValue;
+        this.suitValue = suitValue;
+    }
+
+    public String toString(){
+        return this.suitValue.toString() + ":" + this.cardValue.toString();
+    }
+
+    public cardValue getCardValue(){
+        return this.cardValue;
+    }
+}
+public enum cardValue {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
+public enum suitValue {
+    CLUBS,
+    DIAMOND,
+    SPADE,
+    HEART
 }
